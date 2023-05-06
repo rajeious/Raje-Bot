b13aab4291bdbc684f69c9aac1c196e7b37cf1eb
rajeious <101664683+rajeious@users.noreply.github.com>
2023-05-06 20:14:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,10 @@ import discord
 import os
 import asyncio
 from discord.ext import commands
-import threading
-from threading import thread
-import time
-import urllib.request
-import requests
 from keep_alive import keep_alive
+from discord import Activity, ActivityType
 
 client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
-
-path = "./server/Lavalink.jar"
 
 @client.event
 async def on_ready():
@@ -20,21 +14,6 @@ async def on_ready():
     await client.change_presence(activity=activity)
     print("Bot is Online!")
     client.loop.create_task(update_activity_status())
-
-def run_lavalink():
-    if os.path.exists(path):
-        os.system("java -jar ./server/Lavalink.jar")
-    else:
-        print("\x1b[31m%s\x1b[0m" % "Lavalink.jar not found!")
-        print("\x1b[34m%s\x1b[0m" % "I will try to download one for you.")
-        print("\x1b[34m%s\x1b[0m" % "If for whatever reason it says that the Jar file is corrupt, you can delete the jar file and try running this again.")
-        response = requests.get("https://cdn.darrennathanael.com/jars/Lavalink.jar", stream=True)
-        with open(path, "wb") as file:
-            for chunk in response.iter_content(chunk_size=1024):
-                if chunk:
-                    file.write(chunk)
-        print("\x1b[32m%s\x1b[0m" % "Lavalink.jar downloaded!")
-        os.system("java -jar ./server/Lavalink.jar")
 
 async def load():
     for filename in os.listdir("./cogs"):
@@ -76,8 +55,7 @@ async def on_guild_remove(guild):
     await client.change_presence(activity=activity, status=discord.Status.dnd)
     
 async def main():
-    threading.Thread(target=run_lavalink).start()
-    time.sleep(20) # wait until lavalink is ready up
+    
     await load()
     await client.start(os.getenv("TOKEN"))
 
